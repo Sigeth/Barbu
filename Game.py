013eedf8a8b1b4daf94209e32b8b0ba00d10d699
Thomas <52868615+Sigeth@users.noreply.github.com>
2021-05-04 11:54:29 +0200

    Added launch, and began calculatePoints and Round

--- Game.py
+++ Game.py
@@ -8,13 +8,16 @@ class Game():
         self.nbPlayers = nbPlayers
         self.nbCards = nbCards
         self.paquet = Paquet()
-        self.players = [Player() for i in range(nbPlayers)]
+        self.players = [Player("Player " + str(i+1)) for i in range(nbPlayers)]
+        self.currentContract = None
         self.draw()
         self.playerToPick = choice(self.players)
-        self.roundNb = len(self.players[0].deck)
+        self.trickNb = len(self.players[0].deck)
+        self.roundNb = len(self.players[0].contracts)
     
     def draw(self):
         self.clearDecks()
+        self.paquet.battre()
         for i in range(self.nbCards//self.nbPlayers):
             for p in self.players:
                 p.take(self.paquet.tirer())
@@ -22,21 +25,54 @@ class Game():
     def clearDecks(self):
         for p in self.players:
             p.deck = []
-
-    def trick(self):
         
+    def launch(self):
+        for p in self.players:
+            p.setName()
+        self.changeContract(self.playerToPick)
+        self.round()
+
+    def changeContract(self, p):
+        self.currentContract = p.chooseContract()
+
+    def trick(self, firstPlayer,roundId):
+        #return winner
+        pass
 
 
-
-
-game = Game()
-for p in game.players:
-    print("Cartes d'un nouveau joueur uwu")
-    for c in p.deck:
-        print(str(c.value) + " de " + c.couleur)
-    print(p.points)
+    def calculatePoints(self, deckThrow, roundId):
+        """
+        @param deckThrow: tuple that contains tuples where first element is the card and the second is the player linked (card, linkedPlayer)
+        """
+        trickColor= deckThrow[0][0].couleur
+        winnerCard= deckThrow[0]
+        for el in deckThrow:
+            if el[0].couleur==trickColor and el[0].point>winnerCard[0].point:
+                    winnerCard=el
+        winner=winnerCard
+        if self.currentContract == "Pli" or self.currentContract == "Salade":
+            winner[1].addPoints(10)
+        if self.currentContract == "Dernier pli" or self.currentContract == "Salade" and roundId==13:
+            winner[1].addPoints(100)
+        if self.currentContract != "Pli" or self.currentContract != "Dernier pli":
+            for  el in deckThrow:
+                card = el[0]
+                if (card.value == "roi" and card.couleur == "coeur") and (self.currentContract == "Roi Barbu" or self.currentContract == "Salade"):
+                    winner[1].addPoints(100)
+                elif (card.value == "dame") and (self.currentContract == "Dames" or self.currentContract == "Salade"):
+                    winner[1].addPoints(25)
+                if (card.couleur == "coeur") and (self.currentContract == "Coeurs" or self.currentContract == "Salade"):
+        pass
         
-for i in range(len(game.playerToPick.contracts)):
-    choice = game.playerToPick.pick()
-    currentContract = Contract(choice)
-    while currentContract.isPlayable:
+    def checkVictory(self):
+        #todo boolean function to check if anyone has won
+        pass
+    
+    def round(self):
+        roundId = 0
+        firstPlayer=self.playerToPick 
+        while roundId > self.trickNb or self.checkVictory():
+            roundId+=1
+            firstPlayer = self.trick(firstPlayer)
+            
+    
