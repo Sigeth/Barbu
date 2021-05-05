ff003a5d7a27f3d57dc0e2d8d6d7dff230064664
Thomas <52868615+Sigeth@users.noreply.github.com>
2021-05-05 18:11:25 +0200

    smurfing the code

--- index.py
+++ index.py
@@ -1,34 +1,3 @@
-#from Game import Game
-#from Paquet import Paquet
-import pygame, sys
-pygame.init()
+from Game import Game
 
-size = width, height = 1280, 720
-bgColor = 255, 255, 255
-launchButtonColor = 138, 43, 226
-
-pygame.display.set_caption("Jeu du Barbu")
-icon = pygame.image.load("src/icon.png")
-pygame.display.set_icon(icon)
-screen = pygame.display.set_mode(size)
-
-"""paquet = Paquet()
-paquet.battre()
-
-card = paquet.tirer()
-cardRect = card.aff.get_rect()
-
-cardRect.move_ip(width//2, height//2)"""
-
-font = pygame.font.Font("src/KGRedHands.ttf", 128)
-widthText, heightText = font.size("LAUNCH")
-launchTxt = font.render("LAUNCH", True, (0, 0, 0))
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-    
-    screen.fill(bgColor)
-    screen.blit(launchTxt, (width//2 - widthText//2, height//2 - heightText//2))
-    #screen.blit(card.aff, cardRect)
-    pygame.display.update()
+game = Game()
